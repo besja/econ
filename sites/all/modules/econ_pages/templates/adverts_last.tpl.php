e58c8f18360b47ae46dabb4023cd7ea954d8f373
besja <besja@yandex.ru>
2015-09-23 09:46:12 +0300

    объявления

---
+++ sites/all/modules/econ_pages/templates/adverts_last.tpl.php
@@ -0,0 +1,12 @@
+<div id="notice-widget" class="notice-widget">
+    <div class='row'>
+        <?php foreach ($nodes as $node):?>
+        <a href="<?php print url('node/'.$node->nid);?>" class="notice-widget__notice cf col-xs-12 col-sm-4">
+        <?php 
+        print theme('image_style', array('style_name' => 'leadimage', 'path' => $node->field_leadimage['und'][0]['uri'], 'alt'=>$node->title), array("attributes"=>array("class"=>array("notice-widget__image"))));
+        ?>
+            <div class="notice-widget__text"><?php print $node->title;?></div>
+        </a>
+     <?php endforeach;?>
+    </div>
+</div>
---
+++ sites/all/themes/econ/templates/node--advert.tpl.php
@@ -0,0 +1,25 @@
+
+<?php if ($teaser) :?>
+ <a href="<?php print url('node/'.$node->nid);?>" class="news-widget__item">
+  <?php if (isset($node->field_leadimage['und'][0]['uri'])):?>
+
+  <div class="news-widget__image" style="background-image: url(<?php print image_style_url('leadimage', $node->field_leadimage['und'][0]['uri']); ?>)"></div> 
+  <?php endif;?>
+  <div class="news-widget__title">
+    <?php print $node->title;?>
+  </div>
+  <div class="news-widget__meta cf">
+      <div class="news-widget__date"><?php print format_date($node->created, $type="custom", $format = "d F Y");?></div>
+  </div>
+</a>
+<?php else:?>
+<?php //drupal_set_message("<pre>".print_R($node,1)."</pre>"); ?>
+<div class="page__lead">
+    <div class="advert__date"><?php print format_date($node->created, $type="custom", $format = "d F Y");?></div>
+</div>
+
+<?php print render($content['body']);?>
+
+<?php endif;?>
+
+

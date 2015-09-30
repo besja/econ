1ddc39aad15a4870f1b8cecf35fe45e11ebd7a32
besja <besja@yandex.ru>
2015-09-30 15:48:10 +0400

    people language

--- sites/all/modules/econ_pages/templates/people.tpl.php
+++ sites/all/modules/econ_pages/templates/people.tpl.php
@@ -1,15 +1,24 @@
+<?php if (count($nodes)):?>
+
+<?php 
+ $total = 2.0;
+ $rows= ceil(count($nodes)/$total);
+?>
+<?php for ($j=0; $j<$rows; $j++):?>
 <div class="row">
-    <?php if (count($nodes)):?>
-        <?php foreach($nodes as $row):?>
-            <div class="col-xs-12 col-sm-6">
-                <?php 
-                $node = node_load($row->nid);
-                $render = node_view($node, "teaser");
-                print render($render);
-                ?>
-            </div>
-        <?php endforeach;?>
-    <?php else:?>
-        <p class="empty">Ничего не найдено. Попробуйте изменить параметры поиска</p>
-    <?php endif;?>
+    <?php for ($k=$total*$j; $k<$total*$j + $total ; $k++) :?>
+        <?php if (isset($nodes[$k])):?>
+        <div class="col-xs-12 col-sm-6">
+            <?php 
+            $node = node_load($nodes[$k]->nid);
+            $render = node_view($node, "teaser");
+            print render($render);
+            ?>
+        </div>
+     <?php endif;?>
+    <?php endfor;?>
  </div>
+<?php endfor;?>
+ <?php else:?>
+    <p class="empty">Ничего не найдено. Попробуйте изменить параметры поиска</p>
+<?php endif;?>

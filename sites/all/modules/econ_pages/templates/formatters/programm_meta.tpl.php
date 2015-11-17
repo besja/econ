b9c08086d29315b6dec00dbef575b1c34b4b65d9
besja <besja@yandex.ru>
2015-11-17 16:14:26 +0400

    панель для типа Программа

--- sites/all/modules/econ_pages/templates/formatters/programm_meta.tpl.php
+++ sites/all/modules/econ_pages/templates/formatters/programm_meta.tpl.php
@@ -1,20 +1,4 @@
 <?php $all_fields_on_my_website = field_info_fields();?>
-<?php if ($teaser) :?>
-<div class="col-xs-12 col-sm-6 admission-card">
-    <a href="<?php print url("node/".$node->nid);?>" class="admission-card__link">
-        <?php if (isset($node->field_leadimage['und'][0]['uri'])):?>
-            <?php 
-            print theme('image_style', array('style_name' => 'thumb291x203', 
-                'path' => $node->field_leadimage['und'][0]['uri'], 'alt'=>$node->title, "attributes"=>array("class"=>"admission-card__image")));
-            ?> 
-        <?php endif;?>
-        <h2 class="admission-card__title"><?php if (isset($node->field_programm_type['und'][0]['taxonomy_term']->name)):?><?php print $node->field_programm_type['und'][0]['taxonomy_term']->name.'. ';?><?php endif;?><?php print $node->title;?></h2>
-    </a>
-    <?php print l("Подробнее", "node/".$node->nid, array("attributes"=>array("class"=>"big-btn")));?>
-</div>
-<?php else:?>
-
-<?php print render($content['field_leadtext']);?>
 <div class="contact">
     <table class="common-module common-table contacts__table">
         <tr class="common-table__row">
@@ -63,16 +47,3 @@
         </tr>
     </table>   
 </div>
-
-<?php print render($content['field_web']);?> 
-
-<?php print render($content['body']);?>
-<?php if (count($node->field_structure_people)):?>
-<h2>Администрация программы</h2>
-<?php print render($content['field_structure_people']);?>
-<?php endif;?>
-<?php if (count($node->field_files)):?>
-<h2>Файлы</h2>
-<?php print render($content['field_files']);?>
-<?php endif;?>
-<?php endif;?>

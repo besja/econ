a2233e3fbeb1c0dc5e0316f9c1b911f9c9ffe7c2
st009226 <i.bessarabova@spbu.ru>
2018-02-15 15:00:36 +0300

    external ссылки в новом окне

--- sites/all/themes/econ/js/custom.js
+++ sites/all/themes/econ/js/custom.js
@@ -2,7 +2,15 @@
 
 (function ($) {
 	 $(function(){
-	 	
+
+    $("a[href^=http]").each(function(){
+      if(this.href.indexOf(location.hostname) == -1) {
+        var href = this.href;
+         $(this).attr({
+            target: "_blank"
+         });
+      }
+    }); 	
 	 	var i = 0;
 	 	var arrow = '<svg width="35" height="35" viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg"><title>left</title><g transform="matrix(-1 0 0 1 33 2)" stroke="#FFF" stroke-width="3" fill="none" fill-rule="evenodd"><path d="M11.558 7.545l6.937 6.937c.783.783.787 2.047 0 2.834l-6.608 6.61"/><ellipse cx="15.735" cy="15.735" rx="15.735" ry="15.735"/></g></svg>'; 
        	var arrow_left = '<div class="common-slider__control common-slider__control--next"><i>'+arrow+'</i></div>'; 
